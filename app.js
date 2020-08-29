54e39f2fd5010fa8009d211777ff06b0943b4610
DuHuaL <469580396@qq.com>
2020-08-29 13:59:42 +0800

    处理静态资源的加载

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 //引入外置路由
 var router = require('./router');
 var app = express();
+//配置静态文件
+app.use('/imgs',express.static('imgs'));
+app.use('/node_modules',express.static('node_modules'));
 //使用模板引擎
 app.engine('html',require('express-art-template'));
 //使用路由
